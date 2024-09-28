f462121e809e7952367690618e8cdfeb1ee85a0a
DelsantiV <vincent.delsanti@gmail.com>
2024-09-28 19:28:27 +0200

    Update PathfindingTest.cs
    Fixed some bugs

--- Entities/PathfindingTest.cs
+++ Entities/PathfindingTest.cs
@@ -9,28 +9,15 @@ public class PathfindingTest : MonoBehaviour {
     [SerializeField] LayerMask obstacles;
     [SerializeField] GameObject Target;
     [SerializeField] float gridSize;
-    [SerializeField] int radius;
+    bool existPath;
+
     private void Start()
     {
         Vector3 targetPosition = Target.transform.position;
         pathfinder = new Pathfinder<Vector3>(GetDistance, GetNeighbourNodes);
-        Debug.Log(pathfinder.GenerateAstarPath(transform.position, targetPosition, out path));
-        foreach(Vector3 node in path) { Debug.Log(node); }
+        existPath = pathfinder.GenerateAstarPath(GetClosestNode(transform.position), GetClosestNode(targetPosition), out path);
+        foreach (var node in path) { Debug.Log(node); }
     }
-    /*
-    private void Update()
-    {
-        if (Input.GetMouseButtonDown(0)) //check for a new target
-        {
-            Vector3 target = Camera.main.ScreenToWorldPoint(Input.mousePosition);
-            var _path = new Vector3[0];
-            if (pathfinder.GenerateAstarPath(transform.position, target, out path)) { path = new List<Vector3>(_path); }
-            //if there is a path from current position to target position reassign path.
-        }
-        transform.position = path[0]; //go to next node
-        path.RemoveAt(0); //remove the node from path
-    }
-    */
 
     private void Update()
     {
@@ -39,7 +26,7 @@ public class PathfindingTest : MonoBehaviour {
 
     private void NavigateToTarget(Vector3 targetPosition)
     {
-        if (pathfinder.GenerateAstarPath(transform.position, targetPosition, out path))
+        if (existPath)
         {
             if (path.Count > 0)
             {
@@ -64,15 +51,14 @@ public class PathfindingTest : MonoBehaviour {
         for (int i = -1; i < 2; i++)
         {
             for (int j = -1; j < 2; j++)
-            { 
+            {
                 if (i == 0 && j == 0) continue;
-                Vector3 dir = new Vector3(i, 0, j).normalized*gridSize;
+                Vector3 dir = new Vector3(i, 0, j) * gridSize;
                 if (!Physics.Linecast(pos, pos + dir, obstacles))
                 {
-                    Debug.Log(dir + "  |  " + (pos + dir) + "  |  " + new Vector2 (i,j));
                     neighbours.Add((pos + dir), dir.magnitude);
                 }
-                
+
             }
         }
         return neighbours;
