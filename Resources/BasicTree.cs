c69ca559e595e3a1fc58e8ea31311b44cbeed275
DelsantiV <vincent.delsanti@gmail.com>
2024-12-31 00:26:01 +0100

    Improved Resources
    Added code for further resource improvments : particle system, sound, stump and debris for trees

--- Resources/BasicTree.cs
+++ Resources/BasicTree.cs
@@ -4,14 +4,34 @@ namespace GoTF.Content
 {
     public class BasicTree : BasicResource
     {
+        protected GameObject stump;
+        protected GameObject debris;
+        [SerializeField] protected AudioClip fallingSound;
+        [SerializeField] protected int treeTier;
+
+        public override void Awake()
+        {
+            base.Awake();
+            stump = transform.Find("Stump").gameObject;
+            debris = transform.Find("Debris").gameObject;
+        }
+
         public override void DestroyResource()
         {
             Destroy(gameObject);
+            CreateStumpAndDebris();
+        }
+
+        private void CreateStumpAndDebris()
+        {
+            audioSource.PlayOneShot(fallingSound);
+            Instantiate(stump);
+            Instantiate(debris);
         }
 
         protected override bool CheckToolTierAndType(ItemProperties.DamageProperties damageProperties)
         {
-            if (damageProperties.tier >= 1) { return true; }
+            if (damageProperties.tier >= treeTier) { return true; }
             return false;
         }
     }
