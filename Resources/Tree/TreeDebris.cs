f71da673e284cdbbe7f05d3584d7730ecdd5fc16
DelsantiV <vincent.delsanti@gmail.com>
2025-01-03 19:02:27 +0100

    Update TreeDebris.cs
    Added a random offset for the fall

--- Resources/Tree/TreeDebris.cs
+++ Resources/Tree/TreeDebris.cs
@@ -7,10 +7,14 @@ namespace GoTF.Content
         private AudioSource audioSource;
         [SerializeField] AudioClip fallingSound;
         [SerializeField] AudioClip breakBranchSound;
+        [SerializeField] AudioClip hitGroundSound;
         private void OnEnable()
         {
             Rigidbody rigidbody = GetComponent<Rigidbody>();
-            rigidbody.AddForce(Vector3.forward, ForceMode.Impulse);
+            Vector3 treeOffset = new Vector3(Random.Range(-0.25f, 0.25f), 0, Random.Range(-0.25f, 0.25f));
+            transform.position += treeOffset;
+            Vector3 treeRotation = transform.rotation.eulerAngles + new Vector3(Random.Range(-3f, 3f), 0, Random.Range(-3f, 3f));
+            transform.rotation = Quaternion.Euler(treeRotation);
         }
 
         public void SetActive(bool active)
@@ -19,13 +23,21 @@ namespace GoTF.Content
             if (active)
             {
                 audioSource = GetComponent<AudioSource>();
-                audioSource.PlayOneShot(fallingSound);
+                if (fallingSound != null) audioSource.PlayOneShot(fallingSound);
             }
         }
 
         public void BreakBranch()
         {
-            audioSource.PlayOneShot(breakBranchSound);
+            if (breakBranchSound != null) audioSource.PlayOneShot(breakBranchSound);
+        }
+
+        private void OnCollisionEnter(Collision collision)
+        {
+            if (LayerMask.LayerToName(collision.gameObject.layer) == "Ground")
+            {
+                audioSource.PlayOneShot(hitGroundSound);
+            }
         }
     }
 }
