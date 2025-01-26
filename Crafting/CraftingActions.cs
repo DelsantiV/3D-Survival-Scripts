cbd7dc51926ecc527ed261878ad78552bf19769e
DelsantiV <vincent.delsanti@gmail.com>
2025-01-26 22:28:53 +0100

    Crafting Set up

---
+++ Crafting/CraftingActions.cs
@@ -0,0 +1,25 @@
+using UnityEngine;
+
+namespace GoTF.Content
+{
+    public enum CraftingActions
+    {
+        Strech,
+        Crush
+    }
+    public enum CraftingActionsTwoItems
+    {
+        Assemble,
+        Mix,
+        Knap
+    }
+
+    public enum CraftingProcesses
+    {
+        Cook,
+        Burn,
+        Dry,
+        Moisten,
+        Grind
+    }
+}
--- Crafting/CraftingMenu.cs
+++ Crafting/CraftingMenu.cs
@@ -4,16 +4,6 @@ namespace GoTF.Content
 {
     public class CraftingMenu : BasicUI
     {
-        // Start is called once before the first execution of Update after the MonoBehaviour is created
-        void Start()
-        {
-
-        }
-
-        // Update is called once per frame
-        void Update()
-        {
-
-        }
+        
     }
 }
