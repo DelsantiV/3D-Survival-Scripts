30d718e569ea51484989958c28835fdee9c118b6
DelsantiV <vincent.delsanti@gmail.com>
2025-03-08 19:16:19 +0100

    Added World
    New class to hold world related information

---
+++ World/World.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+namespace GoTF.World
+{
+    public class World
+    {
+        public struct EnvConditions
+        {
+            public float humidity;
+            public float temperature;
+        }
+    }
+}
